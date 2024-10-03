7c47e2b02070ec2326054b4a6283850026f35318
Ashraful05 <jamalrasu420@gmail.com>
2024-10-03 16:46:32 +0600

    cms_page_created_with_model_table

---
+++ database/seeders/CmsPagesSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CmsPage;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CmsPagesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        CmsPage::create([
+            'title'=>'About Us',
+            'description'=>'Content is coming soon',
+            'url'=>'about-us',
+            'meta-title'=>'About Us',
+            'meta_description'=>'About Us Content',
+            'meta_keywords'=>'about us',
+            'status'=>1
+        ]);
+        CmsPage::create([
+            'title'=>'Terms & Conditions',
+            'description'=>'Content is coming soon',
+            'url'=>'terms-conditions',
+            'meta-title'=>'Terms & Conditions',
+            'meta_description'=>'Terms & Conditions Content',
+            'meta_keywords'=>'terms conditions',
+            'status'=>1
+        ]);
+    }
+}
